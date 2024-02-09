c37fc256ec3dda7acaa296b6a414a5eeb20a7e8b
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-02-09 21:02:35 +0530

    Initial Commit

---
+++ AE-Feed/AE_Feed.h
@@ -0,0 +1,18 @@
+//
+//  AE_Feed.h
+//  AE-Feed
+//
+//  Created by archana racha on 09/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AE_Feed.
+FOUNDATION_EXPORT double AE_FeedVersionNumber;
+
+//! Project version string for AE_Feed.
+FOUNDATION_EXPORT const unsigned char AE_FeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AE_Feed/PublicHeader.h>
+
+
