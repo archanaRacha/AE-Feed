6a26da44acc8b91690f10d9f613fe673904c2e95
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-02-12 11:01:26 +0530

    replace stubbing by capturing values instead

--- AE-FeedTests/RemoteFeedLoaderTests.swift
+++ AE-FeedTests/RemoteFeedLoaderTests.swift
@@ -6,9 +6,49 @@
 //
 
 import XCTest
-
+import AE_Feed
 final class RemoteFeedLoaderTests: XCTestCase {
 
+    func test_init_doesNotRequestDataFromURL(){
+        let (_,client) = makeSUT(url:URL.init(string: "https://a-url.com")!)
+        XCTAssertTrue(client.requestedURLs.isEmpty)
+    }
+    func test_load_requestsDataFromURL(){
+        let url = URL.init(string: "https:a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        sut.load()
+        sut.load()
+        XCTAssertEqual(client.requestedURLs,[url,url])
+    }
+    func test_load_deliversErrorOnClientError(){
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedLoader.Error]()
+        sut.load {
+            capturedErrors.append($0)
+        }
+        let clientError = NSError(domain: "Test", code: 0)
+        client.completions[0](clientError)
+        
+        XCTAssertEqual(capturedErrors, [.connectivity])
+    }
+
+    // MARK: helpers
+    private func makeSUT(url: URL = URL.init(string: "https://a-url.com")!) -> (sut:RemoteFeedLoader,client:HTTPClientSpy){
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(client: client, url: url)
+        return(sut, client)
+    }
+    private class HTTPClientSpy:HTTPClient{
+        var requestedURLs : [URL] = []
+
+        var completions = [(Error)->Void]()
+        func get(from url:URL,completion:@escaping(Error)->Void){
+
+            completions.append(completion)
+            requestedURLs.append(url)
+        }
+    }
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
--- FeedAPI/RemoteFeedLoader.swift
+++ FeedAPI/RemoteFeedLoader.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+public protocol HTTPClient{
+    func get(from url: URL, completion:@escaping(Error)->Void)
+}
+public final class RemoteFeedLoader{
+    let client : HTTPClient
+    let url : URL
+    public enum Error: Swift.Error{
+        case connectivity
+    }
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    public func load(completion:@escaping(RemoteFeedLoader.Error) -> Void = { _ in }){
+        client.get(from: url) { error in
+            completion(.connectivity)
+        }
+    }
+}
