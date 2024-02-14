074f1d12cea1fbdb4627f0e568d9d26a60776aea
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-02-14 11:00:05 +0530

    delivers invalid data error on on200 HTTP request

--- FeedAPI/RemoteFeedLoader.swift
+++ FeedAPI/RemoteFeedLoader.swift
@@ -7,21 +7,26 @@
 
 import Foundation
 public protocol HTTPClient{
-    func get(from url: URL, completion:@escaping(Error)->Void)
+    func get(from url: URL, completion:@escaping(Error?,HTTPURLResponse?)->Void)
 }
 public final class RemoteFeedLoader{
     let client : HTTPClient
     let url : URL
     public enum Error: Swift.Error{
         case connectivity
+        case invalidData
     }
     public init(client: HTTPClient, url: URL) {
         self.client = client
         self.url = url
     }
     public func load(completion:@escaping(RemoteFeedLoader.Error) -> Void){
-        client.get(from: url) { error in
-            completion(.connectivity)
+        client.get(from: url) { error , response in
+            if response != nil{
+                completion(.invalidData)
+            }else{
+                completion(.connectivity)
+            }
         }
     }
 }
