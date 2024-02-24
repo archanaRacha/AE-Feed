53c2a756c84f2f69e98a58ef6d525bdbf2af48a2
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-02-24 22:02:03 +0530

    move http client to its own file

---
+++ FeedAPI/FeedItemMapper.swift
@@ -0,0 +1,8 @@
+//
+//  FeedItemMapper.swift
+//  AE-Feed
+//
+//  Created by archana racha on 19/02/24.
+//
+
+import Foundation
---
+++ FeedAPI/HTTPClient.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPClient.swift
+//  AE-Feed
+//
+//  Created by archana racha on 19/02/24.
+//
+
+import Foundation
