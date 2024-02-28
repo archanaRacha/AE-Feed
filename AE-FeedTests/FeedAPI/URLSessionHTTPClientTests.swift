4b30d97430ec5c405a70c7b8a52afd36cf282903
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-02-28 22:28:03 +0530

    remove redundent test

--- AE-FeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ AE-FeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -7,8 +7,49 @@
 
 import XCTest
 
-final class URLSessionHTTPClienTests: XCTestCase {
+class URLSessionHTTPClient {
+    private let session:URLSession
+    init(session:URLSession){
+        self.session = session
+    }
+    func get(from url:URL) {
+        session.dataTask(with: url) { _, _, _ in }.resume()
+    }
+}
+class URLSessionHTTPClientTests: XCTestCase {
 
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string:"http://any-url.com")!
+        let session = URLSessionSpy()
+        let task = URLSessionDataTaskSpy()
+        session.stub(url:url,task:task)
+        let sut = URLSessionHTTPClient(session:session)
+        sut.get(from:url)
+        XCTAssertEqual(task.resumeCount, 1)
+    }
+    
+    //MARK: Helpers
+    private class URLSessionSpy : URLSession {
+
+        private var stubs = [URL:URLSessionDataTask]()
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            return stubs[url] ?? FakeURLSessionDataTask()// data task
+        }
+        func stub(url:URL,task:URLSessionDataTask){
+            stubs[url] = task
+        }
+    }
+    
+    private class FakeURLSessionDataTask : URLSessionDataTask{
+        override func resume() {}
+    }
+    private class URLSessionDataTaskSpy: URLSessionDataTask{
+        var resumeCount = 0
+        override func resume() {
+            resumeCount += 1
+        }
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
