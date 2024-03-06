0d66c4a9920d82a4081df7b9afabb06bdf05cb97
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-03-06 10:45:56 +0530

    move the URLSessionHTTPClient (the system under test or 'SUT') creation to a factory method to protect our tests form breaking changes.

--- AE-FeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ AE-FeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -32,7 +32,7 @@ class URLSessionHTTPClientTests: XCTestCase {
             XCTAssertEqual(request.httpMethod, "GET")
             exp.fulfill()
         }
-        URLSessionHTTPClient().get(from: url) { _ in
+        makeSUT().get(from: url) { _ in
         }
         wait(for: [exp], timeout: 1.0)
         
@@ -43,9 +43,8 @@ class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "any error", code: 1)
        
         URLProtocolStub.stub(data:nil,response:nil, error: error)
-        let sut = URLSessionHTTPClient()
         let exp = expectation(description: "wait for complete")
-        sut.get(from:url) { result  in
+        makeSUT().get(from:url) { result  in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -59,7 +58,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
     }
+    
     //MARK: Helpers
+    private func makeSUT() ->URLSessionHTTPClient{
+        return URLSessionHTTPClient()
+    }
     private class URLProtocolStub : URLProtocol {
         
         private struct Stub{
