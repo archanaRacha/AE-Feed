98a8ca911d7ac07485d3892026e493ff25da6211
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-03-06 10:55:36 +0530

    extract memory leaking method and add it to sharing file

--- AE-FeedTests/FeedAPI/Helper/XCTestCase+MemoryLeakTracking.swift
+++ AE-FeedTests/FeedAPI/Helper/XCTestCase+MemoryLeakTracking.swift
@@ -5,4 +5,12 @@
 //  Created by archana racha on 06/03/24.
 //
 
-import Foundation
+import XCTest
+
+extension XCTestCase{
+     func trackMemoryLeaks(_ instance:AnyObject,file:StaticString = #file,line:UInt = #line){
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance,"instance should have been deallocated. Potential memory leak.",file: file, line:  line)
+        }
+    }
+}
--- AE-FeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ AE-FeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -65,11 +65,7 @@ class URLSessionHTTPClientTests: XCTestCase {
         trackMemoryLeaks(sut,file:file,line:line)
         return sut
     }
-    private func trackMemoryLeaks(_ instance:AnyObject,file:StaticString = #file,line:UInt = #line){
-        addTeardownBlock { [weak instance] in
-            XCTAssertNil(instance,"instance should have been deallocated. Potential memory leak.",file: file, line:  line)
-        }
-    }
+    
     private class URLProtocolStub : URLProtocol {
         
         private struct Stub{
