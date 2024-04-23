f21e3ee563b4f354bacd752ee7951d15cc963a1f
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-04-23 17:55:49 +0530

    extract system under test creation to a factory method to protect tests against breaking changes

--- AE-FeedTests/FeedCache/CacheFeedUserCaseTests.swift
+++ AE-FeedTests/FeedCache/CacheFeedUserCaseTests.swift
@@ -23,28 +23,28 @@ class FeedStore {
     }
 }
 final class CacheFeedUserCaseTests: XCTestCase {
-
+    
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = FeedStore()
-        _ = LocalFeedLoader(store:store)
+        let (_,store) = makeSUT()
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
     
     func test_save_requestsCacheDeletion(){
-        let store = FeedStore()
-        let sut = LocalFeedLoader(store:store)
+        
+        let (sut,store) = makeSUT()
         let items = [uniqueItem(),uniqueItem()]
         sut.save(items)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
+    
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
@@ -52,15 +52,20 @@ final class CacheFeedUserCaseTests: XCTestCase {
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
     }
-
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
     //MARK: Helpers
+    private func makeSUT() -> (sut:LocalFeedLoader, store: FeedStore ){
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store:store)
+        return (sut, store)
+    }
     private func uniqueItem() -> FeedItem{
         return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
     }
