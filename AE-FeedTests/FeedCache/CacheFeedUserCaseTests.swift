098e5914fcb4144f70ac6a0a2825d91626cb9378
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-04-23 17:38:59 +0530

    doesn't delete cache upon LocalFeedLoader creation

--- AE-FeedTests/FeedCache/CacheFeedUserCaseTests.swift
+++ AE-FeedTests/FeedCache/CacheFeedUserCaseTests.swift
@@ -7,8 +7,22 @@
 
 import XCTest
 
+class LocalFeedLoader{
+    init(store:FeedStore){
+        
+    }
+}
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
 final class CacheFeedUserCaseTests: XCTestCase {
 
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store:store)
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
