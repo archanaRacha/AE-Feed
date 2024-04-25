6a8f104d31ea44a54b53e374fc00b2da5bf3f912
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-04-25 10:52:20 +0530

    Extract the cache insertion into a helper function to make logic inside closure callbacks easier to follow

--- AE-FeedTests/FeedCache/CacheFeedUserCaseTests.swift
+++ AE-FeedTests/FeedCache/CacheFeedUserCaseTests.swift
@@ -20,13 +20,16 @@ class LocalFeedLoader{
             if let cacheDeletionerror = error {
                 completion(cacheDeletionerror)
             }else{
-                self.store.insert(items,timestamp: self.currentDate()){ [weak self] error in
-                    guard let self = self else {return}
-                    completion(error)
-                }
+                self.cache(items,with:completion)
                 
             }
             
+        }
+    }
+    private func cache(_ items:[FeedItem],with completion:@escaping (Error?) -> Void){
+        store.insert(items,timestamp: currentDate()){ [weak self] error in
+            guard let self = self else {return}
+            completion(error)
         }
     }
 }
