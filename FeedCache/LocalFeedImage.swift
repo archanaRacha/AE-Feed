ad27134cd8719efe9c0786c4e498fa15a950833e
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-10 15:48:06 +0530

    Replace load command completion to return a result type rather than an optional error.

---
+++ FeedCache/LocalFeedImage.swift
@@ -0,0 +1,8 @@
+//
+//  LocalFeedImage.swift
+//  AE-Feed
+//
+//  Created by archana racha on 10/05/24.
+//
+
+import Foundation
---
+++ FeedFeature/FeedImage.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImage.swift
+//  AE-Feed
+//
+//  Created by archana racha on 10/05/24.
+//
+
+import Foundation
