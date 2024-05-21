e5ed517136a6980d3976cfcbec14fbd51b34a0c1
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-21 21:46:20 +0530

    Extract duplicate test helpers into a shared scope

---
+++ AE-FeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  FeedCacheTestHelpers.swift
+//  AE-FeedTests
+//
+//  Created by archana racha on 21/05/24.
+//
+
+import Foundation
---
+++ AE-FeedTests/Helper/SharedTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  SharedTestHelpers.swift
+//  AE-FeedTests
+//
+//  Created by archana racha on 21/05/24.
+//
+
+import Foundation
