341114f91cbac731239000d576435edbbc30d2a7
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-21 10:29:35 +0530

    LocalFeedLoader does not message store upon creation (before validating the cached feed)

---
+++ AE-FeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -0,0 +1,48 @@
+//
+//  ValidateFeedCacheUseCaseTests.swift
+//  AE-FeedTests
+//
+//  Created by archana racha on 21/05/24.
+//
+
+import XCTest
+import AE_Feed
+
+final class ValidateFeedCacheUseCaseTests: XCTestCase {
+
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_,store) = makeSUT()
+        XCTAssertEqual(store.receivedMessages , [])
+    }
+    //MARK: Helpers
+    private func makeSUT(currentDate:@escaping() -> Date = Date.init,file:StaticString = #file, line : UInt = #line) -> (sut:LocalFeedLoader, store: FeedStoreSpy ){
+        let store = FeedStoreSpy()
+        let sut = LocalFeedLoader(store:store,currentDate: currentDate)
+        trackMemoryLeaks(store,file: file,line: line)
+        trackMemoryLeaks(sut,file: file,line: line)
+        return (sut, store)
+    }
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
