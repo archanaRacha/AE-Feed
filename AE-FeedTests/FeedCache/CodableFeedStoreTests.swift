8feb6b826cf0a5e758b24c508f4945897fb31cfd
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-22 22:13:37 +0530

    Add memory leak tracking

--- AE-FeedTests/FeedCache/CodableFeedStoreTests.swift
+++ AE-FeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -124,8 +124,11 @@ final class CodableFeedStoreTests: XCTestCase {
         }
     }
     // _ MARK: Helpers
-    private func makeSUT() -> CodableFeedStore{
-        return CodableFeedStore()
+    private func makeSUT(file: StaticString = #file, line:UInt= #line) -> CodableFeedStore{
+        let sut = CodableFeedStore()
+        trackMemoryLeaks(sut, file: file,line:line)
+        return sut
+        
     }
 
 }
