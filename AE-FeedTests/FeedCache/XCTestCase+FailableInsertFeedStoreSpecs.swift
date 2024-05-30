05506eda9f4cf2ef7f057af82dbed1acc4c694a9
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-30 16:39:43 +0530

    Extract FailableInsertFeedStoreSpecs checks into reusable assertions

--- AE-FeedTests/FeedCache/XCTestCase+FailableInsertFeedStoreSpecs.swift
+++ AE-FeedTests/FeedCache/XCTestCase+FailableInsertFeedStoreSpecs.swift
@@ -6,30 +6,20 @@
 //
 
 import XCTest
+import AE_Feed
 
-final class XCTestCase_FailableInsertFeedStoreSpecs: XCTestCase {
+extension FailableInsertFeedStoreSpecs where Self: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    func assertThatInsertDeliversErrorOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let insertionError = insert(cache:(uniqueImageFeed().local, Date()), to: sut)
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+            XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error", file: file, line: line)
         }
-    }
+
+        func assertThatInsertHasNoSideEffectsOnInsertionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+            insert(cache:(uniqueImageFeed().local, Date()), to: sut)
+
+            expect(sut, toRetrieve: .empty, file: file, line: line)
+        }
 
 }
