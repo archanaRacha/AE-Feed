b20f9da1dc0d1d83a65b2aca682b23b233c3a743
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-30 18:15:35 +0530

    Add empty CoreData feed store specs

--- AE-FeedTests/FeedCache/CoreDataFeedStoreTests.swift
+++ AE-FeedTests/FeedCache/CoreDataFeedStoreTests.swift
@@ -7,8 +7,45 @@
 
 import XCTest
 
-final class CoreDataFeedStoreTests: XCTestCase {
-
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
