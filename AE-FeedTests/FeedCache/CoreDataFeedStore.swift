3b311960d47ef8790d36a1000154620848c0f737
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-30 18:44:48 +0530

    Add ManagedCache and ManagedFeedImage model representations

--- AE-FeedTests/FeedCache/CoreDataFeedStore.swift
+++ AE-FeedTests/FeedCache/CoreDataFeedStore.swift
@@ -7,20 +7,30 @@
 
 import Foundation
 import AE_Feed
+import CoreData
 
 public final class CoreDataFeedStore : FeedStore {
     public init() {}
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
     }
-    
     public func insert(_ items: [AE_Feed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletions) {
         
     }
-    
     public func retrieve(completion: @escaping RetrievalCompletions) {
         completion(.empty)
     }
-    
-    
+}
+
+private class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+}
+
+private class ManagedFeedImage: NSManagedObject {
+    @NSManaged var id: UUID
+    @NSManaged var imageDescription: String?
+    @NSManaged var location: String?
+    @NSManaged var url: URL
+    @NSManaged var cache: ManagedCache
 }
