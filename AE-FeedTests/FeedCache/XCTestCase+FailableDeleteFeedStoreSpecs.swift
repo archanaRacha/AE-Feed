35d851314f8ba2a25ab3656524ba67ee8d53c002
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-05-30 16:42:58 +0530

    Extract FailableDeleteFeedStoreSpecs checks into reusable assertions

--- AE-FeedTests/FeedCache/XCTestCase+FailableDeleteFeedStoreSpecs.swift
+++ AE-FeedTests/FeedCache/XCTestCase+FailableDeleteFeedStoreSpecs.swift
@@ -6,30 +6,19 @@
 //
 
 import XCTest
+import AE_Feed
 
-final class XCTestCase_FailableDeleteFeedStoreSpecs: XCTestCase {
+extension FailableDeleteFeedStoreSpecs where Self: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: FeedStore, file: StaticString = #file, line: UInt = #line) {
+        deleteCache(from: sut)
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        expect(sut, toRetrieve: .empty, file: file, line: line)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
