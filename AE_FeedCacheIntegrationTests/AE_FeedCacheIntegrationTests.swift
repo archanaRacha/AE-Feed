951c441ebbd9b073e1d2b4265510f419c167d48d
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-06-14 10:59:52 +0530

    LocalFeedLoader in integration with the CoreDataFeedStore delivers latest items saved on separate instances, proving we correctly persist the data models to disk.

--- AE_FeedCacheIntegrationTests/AE_FeedCacheIntegrationTests.swift
+++ AE_FeedCacheIntegrationTests/AE_FeedCacheIntegrationTests.swift
@@ -49,6 +49,39 @@ final class AE_FeedCacheIntegrationTests: XCTestCase {
         }
         wait(for: [loadExp], timeout: 1.0)
     }
+    func test_save_orverridesItemsSavedOnASeparateInstance(){
+        let sutToPerformFirstSave = makeSUT()
+        let sutToPerformLastSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let firstFeed = uniqueImageFeed().models
+        let latestFeed = uniqueImageFeed().models
+        save(firstFeed, with:sutToPerformFirstSave)
+        save(latestFeed,with: sutToPerformLastSave)
+        expect(sutToPerformLoad, toLoad: latestFeed)
+    }
+    private func save(_ feed:[FeedImage], with loader:LocalFeedLoader,file:StaticString = #file,line:UInt = #line){
+        let saveExp = expectation(description: "wait for save comppletion")
+        loader.save(feed) { saveError in
+            XCTAssertNil(saveError, "Expected to save feed successfully", file: file, line: line)
+                        saveExp.fulfill()
+        }
+        wait(for:[saveExp],timeout: 1.0)
+        
+    }
+    private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #file, line: UInt = #line){
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result{
+            case let .success(loadedFeed):
+                XCTAssertEqual(loadedFeed, expectedFeed,file:file,line: line)
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead", file: file, line: line)
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp],timeout: 1.0)
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         setupEmptyStoreState()
