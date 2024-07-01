64b535fc068bfd44cba19866de63e2ece92c043a
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-07-01 11:19:23 +0530

    Nest LoadFeedResult into the FeedLoader protocol as `FeedLoader.Result` since they're closely related

--- FeedAPI/RemoteFeedLoader.swift
+++ FeedAPI/RemoteFeedLoader.swift
@@ -14,7 +14,7 @@ public final class RemoteFeedLoader : FeedLoader{
         case connectivity
         case invalidData
     }
-    public typealias Result = LoadFeedResult
+    public typealias Result = FeedLoader.Result
     
     public init(client: HTTPClient, url: URL) {
         self.client = client
