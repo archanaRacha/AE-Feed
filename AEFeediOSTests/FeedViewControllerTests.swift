c8ba88ca2339be172916821ec299c01a3478c86d
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-07-12 12:37:43 +0530

    Does not load feed on init (before the view is loaded)

---
+++ AEFeediOSTests/FeedViewControllerTests.swift
@@ -0,0 +1,58 @@
+//
+//  FeedViewControllerTests.swift
+//  Prototype
+//
+//  Created by archana racha on 12/07/24.
+//
+
+import XCTest
+
+final class FeedViewcontroller {
+    init(loader: FeedViewControllerTests.LoaderSpy){
+        
+    }
+}
+final class FeedViewControllerTests: XCTestCase {
+
+    func test_init_doesNotLoadFeed() {
+        let loader = LoaderSpy()
+        let sut = FeedViewcontroller(loader:loader)
+        XCTAssertEqual(loader.loadCallCount, 0 )
+    }
+    
+    // MARK: Helpers
+    
+    class LoaderSpy {
+        private(set) var loadCallCount : Int = 0
+    }
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
