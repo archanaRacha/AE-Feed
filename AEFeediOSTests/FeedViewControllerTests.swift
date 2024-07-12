a43648a7c94caace42b60c84f3758102b3747ab9
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-07-12 12:45:33 +0530

    Load feed on view did load

--- AEFeediOSTests/FeedViewControllerTests.swift
+++ AEFeediOSTests/FeedViewControllerTests.swift
@@ -6,24 +6,40 @@
 //
 
 import XCTest
+import UIKit
 
-final class FeedViewcontroller {
-    init(loader: FeedViewControllerTests.LoaderSpy){
-        
+final class FeedViewcontroller : UIViewController {
+    private var loader : FeedViewControllerTests.LoaderSpy?
+    convenience init(loader: FeedViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loader?.load()
     }
 }
 final class FeedViewControllerTests: XCTestCase {
 
     func test_init_doesNotLoadFeed() {
         let loader = LoaderSpy()
-        let sut = FeedViewcontroller(loader:loader)
+        _ = FeedViewcontroller(loader:loader)
         XCTAssertEqual(loader.loadCallCount, 0 )
     }
     
+    func test_viewDidLoad_loadsFeed(){
+        let loader = LoaderSpy()
+        let sut = FeedViewcontroller(loader: loader)
+        sut.loadViewIfNeeded()
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
     // MARK: Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount : Int = 0
+        func load(){
+            loadCallCount += 1
+        }
     }
     
     override func setUpWithError() throws {
@@ -39,20 +55,20 @@ final class FeedViewControllerTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testExample() throws {
+//        // UI tests must launch the application that they test.
+//        let app = XCUIApplication()
+//        app.launch()
+//
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
