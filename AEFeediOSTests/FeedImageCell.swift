08b75e03a33f7df93562217f67cef41c7cf5a760
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-08-24 17:32:20 +0530

    Rended Loaded field on success full load completion

---
+++ AEFeediOSTests/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  AEFeediOSTests
+//
+//  Created by archana racha on 21/08/24.
+//
+
+import UIKit
+
+class FeedImageCell: UITableViewCell {
+
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
