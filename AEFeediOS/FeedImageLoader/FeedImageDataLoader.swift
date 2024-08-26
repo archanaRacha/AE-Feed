26120689f3189872cac681806a934bb8053d4409
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-08-26 13:00:45 +0530

    Extract UI classes to production files and UIImage test helper to a separate file

---
+++ AEFeediOS/FeedImageLoader/FeedImageDataLoader.swift
@@ -0,0 +1,8 @@
+//
+//  FeedImageDataLoader.swift
+//  AEFeediOS
+//
+//  Created by archana racha on 26/08/24.
+//
+
+import Foundation
---
+++ AEFeediOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+TestHelpers.swift
+//  AEFeediOSTests
+//
+//  Created by archana racha on 26/08/24.
+//
+
+import Foundation
