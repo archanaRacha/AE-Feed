75c9431ca298d6fac80050e882fd72fc982a1afc
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-08-27 11:05:49 +0530

    Remove mutable state from FeedViewModel. The FeedViewModel only needs to forward state changes, so state is only transient.

--- AEFeediOS/FeedUI/Controllers/FeedRefreshViewController.swift
+++ AEFeediOS/FeedUI/Controllers/FeedRefreshViewController.swift
@@ -8,26 +8,48 @@
 import UIKit
 import AE_Feed
 
-final class FeedRefreshViewController : NSObject {
-    private(set) lazy var view: UIRefreshControl = {
-        let view = UIRefreshControl()
-        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
-        return view
-    }()
-    private let feedLoader : FeedLoader
-    init(feedLoader:FeedLoader) {
+final class FeedViewModel {
+    typealias Observer<T> = (T) -> Void
+    private var feedLoader: FeedLoader
+    init(feedLoader:FeedLoader){
         self.feedLoader = feedLoader
     }
-    var onRefresh:(([FeedImage])->Void)?
-    
-    @objc func refresh() {
-        view.beginRefreshing()
+    var onLoadingStateChange:Observer<Bool>?
+    var onFeedLoad:Observer<[FeedImage]>?
+   
+    func loadFeed() {
+        onLoadingStateChange?(true)
         feedLoader.load { [weak self] result in
             if let feed = try? result.get(){
-                self?.onRefresh?(feed)
+                self?.onFeedLoad?(feed)
             }
-            self?.view.endRefreshing()
+            self?.onLoadingStateChange?(false)
         }
+    }
+}
+final class FeedRefreshViewController : NSObject {
+    
+    private(set) lazy var view: UIRefreshControl = binded(UIRefreshControl())
+    
+    private let viewModel : FeedViewModel
+    
+    init(viewModel:FeedViewModel) {
+        self.viewModel = viewModel
+    }
+    @objc func refresh() {
+        viewModel.loadFeed()
+        
+    }
+    func binded(_ view: UIRefreshControl) -> UIRefreshControl{
+        viewModel.onLoadingStateChange = {[weak view] isLoading in
+            if isLoading{
+                view?.beginRefreshing()
+            }else{
+               view?.endRefreshing()
+            }
+        }
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
     }
 }
 
