4d0a3d3a383fd6c0cc2240f8131305c307b62a0d
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-09-13 13:25:02 +0530

    Move WeakRefVirtualProxy to separate file

--- AEFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ AEFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -53,20 +53,4 @@ final class FeedViewAdapter : FeedView{
     
 }
 
-private final class WeakRefVirtualProxy<T:AnyObject> {
-    private weak var object: T?
-    init(object: T) {
-        self.object = object
-    }
-}
 
-extension WeakRefVirtualProxy:FeedLoadingView where T : FeedLoadingView{
-    func display(_ viewModel: FeedLoadingViewModel) {
-        object?.display(viewModel)
-    }
-}
-extension WeakRefVirtualProxy:FeedImageView where T : FeedImageView, T.Image == UIImage{
-    func display(_ model: FeedImageViewModel<UIImage>) {
-        object?.display(model)
-    }
-}
---
+++ AEFeediOS/FeedUI/Composers/WeakRefVirtualProxy.swift
@@ -0,0 +1,27 @@
+//
+//  WeakRefVirtualProxy.swift
+//  AEFeediOS
+//
+//  Created by archana racha on 13/09/24.
+//
+
+import Foundation
+import UIKit
+
+final class WeakRefVirtualProxy<T:AnyObject> {
+    private weak var object: T?
+    init(object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy:FeedLoadingView where T : FeedLoadingView{
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+extension WeakRefVirtualProxy:FeedImageView where T : FeedImageView, T.Image == UIImage{
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
