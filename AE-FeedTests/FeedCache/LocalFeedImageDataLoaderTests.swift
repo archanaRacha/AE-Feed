f13a872ed16cbb1154def6e1e7185611eb1996eb
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-05 16:09:50 +0530

    LocalFeedImageDataLoader.loadImageData delivers not found error when store can't find image data for url

--- AE-FeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
+++ AE-FeedTests/FeedCache/LocalFeedImageDataLoaderTests.swift
@@ -20,6 +20,7 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
     }
     public enum Error: Swift.Error {
         case failed
+        case notFound
     }
     private let store: FeedImageDataStore
 
@@ -29,7 +30,9 @@ final class LocalFeedImageDataLoader: FeedImageDataLoader {
 
     func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
         store.retrieve(dataForURL: url) { result in
-            completion(.failure(Error.failed))
+            completion(result
+                    .mapError { _ in Error.failed }
+                    .flatMap { _ in .failure(Error.notFound) })
         }
         return Task()
     }
@@ -58,6 +61,13 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
             store.complete(with: retrievalError)
         })
     }
+    func test_loadImageDataFromURL_deliversNotFoundErrorOnNotFound() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: notFound(), when: {
+            store.complete(with: .none)
+        })
+    }
     // MARK: - Helpers
 
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedImageDataLoader, store: StoreSpy) {
@@ -66,6 +76,9 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
         trackMemoryLeaks(store, file: file, line: line)
         trackMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
+    }
+    private func notFound() -> FeedImageDataLoader.Result {
+        return .failure(LocalFeedImageDataLoader.Error.notFound)
     }
     private func failed() -> FeedImageDataLoader.Result {
         return .failure(LocalFeedImageDataLoader.Error.failed)
@@ -107,6 +120,9 @@ class LocalFeedImageDataLoaderTests: XCTestCase {
 
         func complete(with error: Error, at index: Int = 0) {
             completions[index](.failure(error))
+        }
+        func complete(with data: Data?, at index: Int = 0) {
+            completions[index](.success(data))
         }
     }
 
