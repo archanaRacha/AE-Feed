51171ee71b70ac4ec17f2c857de59b19c2d77301
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-05 16:44:43 +0530

    Extract FeedImageDataStoreSpy into a shared scope

---
+++ AE-FeedTests/Helper/FeedImageDataStoreSpy.swift
@@ -0,0 +1,32 @@
+//
+//  FeedImageDataStoreSpy.swift
+//  AE-FeedTests
+//
+//  Created by archana racha on 05/10/24.
+//
+
+import Foundation
+import AE_Feed
+
+class FeedImageDataStoreSpy: FeedImageDataStore {
+    enum Message: Equatable {
+        case insert(data: Data, for: URL)
+        case retrieve(dataFor: URL)
+    }
+    private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    private(set) var receivedMessages = [Message]()
+    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+        receivedMessages.append(.insert(data: data, for: url))
+    }
+    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        receivedMessages.append(.retrieve(dataFor: url))
+        retrievalCompletions.append(completion)
+    }
+
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        retrievalCompletions[index](.failure(error))
+    }
+    func completeRetrieval(with data: Data?, at index: Int = 0) {
+        retrievalCompletions[index](.success(data))
+    }
+}
