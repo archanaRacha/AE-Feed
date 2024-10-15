8ad777856f837a2f57bf41443115931f5aa7156f
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-10-15 15:06:29 +0530

    LocalFeedLoader.validateCache succeeds on empty cache

--- AE-FeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
+++ AE-FeedTests/FeedCache/ValidateFeedCacheUseCaseTests.swift
@@ -82,6 +82,13 @@ final class ValidateFeedCacheUseCaseTests: XCTestCase {
         store.completeRetrieval(with: anyNSError())
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
+    func test_validateCache_succeedsOnEmptyCache() {
+        let (sut, store) = makeSUT()
+
+        expect(sut, toCompleteWith: .success(()), when: {
+            store.completeRetrievalWithEmptyCache()
+        })
+    }
     //MARK: Helpers
     private func makeSUT(currentDate:@escaping() -> Date = Date.init,file:StaticString = #file, line : UInt = #line) -> (sut:LocalFeedLoader, store: FeedStoreSpy ){
         let store = FeedStoreSpy()
