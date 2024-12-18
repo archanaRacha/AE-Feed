b1e9fd84c7c019b14366ae7ebc72c9a1a75f9ef7
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-12-18 12:01:49 +0530

    Add ImageComment data model

---
+++ AEFeedAPI/ImageComments.swift
@@ -0,0 +1,22 @@
+//
+//  ImageComments.swift
+//  AEFeedAPI
+//
+//  Created by archana racha on 18/12/24.
+//
+
+import Foundation
+
+public struct ImageComment: Equatable {
+    public let id: UUID
+    public let message: String
+    public let createdAt: Date
+    public let username: String
+
+    public init(id: UUID, message: String, createdAt: Date, username: String) {
+        self.id = id
+        self.message = message
+        self.createdAt = createdAt
+        self.username = username
+    }
+}
