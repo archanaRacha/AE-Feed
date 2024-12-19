edc5ca99b7e3c2e0af89344729a4e186f2ab99f4
archanaRacha <3062400+archanaRacha@users.noreply.github.com>
2024-12-19 11:50:03 +0530

    Test FeedImageDataMapper in isolation

---
+++ AEFeedAPI/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  AEFeedAPI
+//
+//  Created by archana racha on 19/12/24.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+
+        return data
+    }
+}
